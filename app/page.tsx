af8179b1bf8ea166a14bc783865510ac2d037ac7
SJ <126138033+sojeong0302@users.noreply.github.com>
2026-06-25 13:26:43 +0900

    feat: useEffect -> tanstack query 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,50 @@
 "use client";
 
 import type { Todo } from "@/types"; //그냥 import 해도되지만 type 붙여주면 가독성⬆️
+import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 
 const page = () => {
-  const [todos, setTodos] = useState<
-    Todo[] //배열인거 표현하고 싶으면 뒤에 대괄호[] 넣기
-  >([]);
+  //   const [todos, setTodos] = useState<
+  //     Todo[] //배열인거 표현하고 싶으면 뒤에 대괄호[] 넣기
+  //   >([]);
 
-  useEffect(() => {
-    const getTodos = async () => {
+  //   useEffect(() => {
+  //     const getTodos = async () => {
+  //       const response = await fetch("http://localhost:4000/todos");
+  //       const data = await response.json();
+
+  //       setTodos(data);
+  //     };
+  //     getTodos();
+  //   }, []);
+
+  const {
+    data: todos,
+    isLoading,
+    isError,
+  } = useQuery<Todo[]>({
+    queryKey: ["todos"],
+    queryFn: async () => {
       const response = await fetch("http://localhost:4000/todos");
       const data = await response.json();
+      return data;
+    },
+  });
 
-      setTodos(data);
-    };
-    getTodos();
-  }, []);
+  if (isLoading) {
+    return <p>로딩 중 입니다 ~.~</p>;
+  }
+
+  if (isError) {
+    return <p>에러가 발생했습니다 ~.~</p>;
+  }
 
   return (
     <div>
       <h1>TODOLIST</h1>
-      {todos.map((todo) => {
+      {todos?.map((todo) => {
+        //만약 없을 수도 있으니까 옵셔널 체이닝[=?] 넣어주기
         return (
           <div key={todo.id} className="border">
             <p>{todo.title}</p>
