61513b2cf220bdcd4ac207690eeaef3bf5859f48
SJ <126138033+sojeong0302@users.noreply.github.com>
2026-06-25 11:23:22 +0900

    feat: 데이터 불러오기(극초반)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+"use client";
+
+import { useEffect, useState } from "react";
 
 const page = () => {
-  return <div>HomePage</div>;
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const getTodos = async () => {
+      const response = await fetch("http://localhost:4000/todos");
+      const data = await response.json();
+
+      setTodos(data);
+    };
+    getTodos();
+  }, []);
+
+  return (
+    <div>
+      <h1>TODOLIST</h1>
+      {todos.map((todo) => {
+        return (
+          <div key={todo.id} className="border">
+            <p>{todo.title}</p>
+            <p>{todo.done ? "완료" : "미완료"}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default page;
