f58ffb5d02b33e97ee5c977c0889604ef2170b39
SJ <126138033+sojeong0302@users.noreply.github.com>
2026-06-25 11:29:45 +0900

    feat: typescript 사용

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import type { Todo } from "@/types"; //그냥 import 해도되지만 type 붙여주면 가독성⬆️
 import { useEffect, useState } from "react";
 
 const page = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState<
+    Todo[] //배열인거 표현하고 싶으면 뒤에 대괄호[] 넣기
+  >([]);
 
   useEffect(() => {
     const getTodos = async () => {
